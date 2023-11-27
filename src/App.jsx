39248941e64159229753f228a2154b22b64cd731
Giselda  Arevalo <96034020+giselarevalo@users.noreply.github.com>
2023-11-27 19:52:06 -0300

    created dashboard with react

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './assets/css/app.css'
+import { ContentWrapper } from './components/ContentWrapper'
+import { SiderBar } from './components/SiderBar'
+
+function App() {
+  
+
+  return (
+    <div id="wrapper">
+
+		<SiderBar/>
+
+		<ContentWrapper/>
+
+
+	</div>
+  )
+}
+
+export default App
